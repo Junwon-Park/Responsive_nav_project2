d994d97ba1b105478ddcbb37823a59c3d6a3d88d
Junwon_Park <upjw0429@gmail.com>
2022-01-03 21:30:53 +0900

    modifiy padding-left: 0 of the logo_title, menu_list, icon_list

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ console.log(toggle);
 toggle.addEventListener('click', () => {
   menuList.classList.toggle('active');
   iconList.classList.toggle('active');
+  // classList의 메서드의 인자는 ''를 사용하고 클래스던 아이디던 .이나 #을 붙이지 않고 이름만 쓴다.
   console.log('Clicked!!!');
   console.log(menuList.classList.item(1));
 });
